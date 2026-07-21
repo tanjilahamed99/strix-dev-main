5670a3f1334f1ed2a29b09e0e67a57abb6ea85e2
tanjilahamed99 <tanjil113355@gmail.com>
2026-07-21 23:32:50 +0600

    ci/cd work well

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -8,7 +8,7 @@ const navLinks = [
     { name: "Services", href: "/services" },
     { name: "Work", href: "/work" },
     { name: "About", href: "/about" },
-    { name: "Contact Test", href: "/contact" },
+    { name: "Contact", href: "/contact" },
 ];
 
 const Navbar = () => {
