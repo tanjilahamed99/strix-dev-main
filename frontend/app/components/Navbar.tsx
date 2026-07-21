d910ce32adb8a4a12680492819070f029b712062
tanjilahamed99 <tanjil113355@gmail.com>
2026-07-21 23:31:08 +0600

    new code added

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -8,7 +8,7 @@ const navLinks = [
     { name: "Services", href: "/services" },
     { name: "Work", href: "/work" },
     { name: "About", href: "/about" },
-    { name: "Contact", href: "/contact" },
+    { name: "Contact Test", href: "/contact" },
 ];
 
 const Navbar = () => {
